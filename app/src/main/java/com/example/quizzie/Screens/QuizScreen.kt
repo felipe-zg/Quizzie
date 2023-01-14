3e1e0ceecc98aec57e5b4579ec310df42d78c2f7
felipe-zg <felipe_zeba@outlook.com>
2023-01-14 00:05:09 -0600

    feat: add action handlers

--- app/src/main/java/com/example/quizzie/Screens/QuizScreen.kt
+++ app/src/main/java/com/example/quizzie/Screens/QuizScreen.kt
@@ -12,64 +12,36 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 import com.example.quizzie.Composables.RadioButtonQuestion
 import com.example.quizzie.Composables.RoundedButton
+import com.example.quizzie.Helpers.QuizzieDatabaseHelper
 import com.example.quizzie.Models.Answer
 import com.example.quizzie.Models.QuestionItem
 import com.example.quizzie.ui.theme.QuizzieTheme
 
-val questions = listOf<QuestionItem>(
-    QuestionItem(
-        "What is the correct way to initialize a nullable variable?",
-        listOf(
-            Answer(1, "val name = null"),
-            Answer(2, "var name: String"),
-            Answer(3, "val name: String"),
-            Answer(4, "val name: String? = null"),
-        ),
-        4
-    ),
-    QuestionItem(
-        "Kotlin interfaces ad abstract classes are very similar. What is one thing abstract class can do that interfaces cannot?",
-        listOf(
-            Answer(1, "Only abstract classes are inheritable by subclasses"),
-            Answer(2, "Only abstract classes can inherit from multiple superclasses"),
-            Answer(3, "Only abstract classes can have abstract methods"),
-            Answer(4, "Only abstract classes can store state reference"),
-        ),
-        4
-    ),
-    QuestionItem(
-        "Your code need to try casting an object. If the cast is not possible, you do not want an exception generated, instead you want null to be assigned. Which operator can safely cast a value?",
-        listOf(
-            Answer(1, "as? (reference)"),
-            Answer(2, "??"),
-            Answer(3, "is"),
-            Answer(4, "as"),
-        ),
-        1
-    ),
-)
-
 @Composable
-fun QuizScreen() {
+fun QuizScreen(navController: NavController) {
     var index by remember { mutableStateOf(0) }
-    var currentQuestion by remember { mutableStateOf(questions[index]) }
+    var currentQuestion by remember { mutableStateOf(QuizzieDatabaseHelper.questions[index]) }
+
+    fun handleNext() {
+        if(index < QuizzieDatabaseHelper.questions.size - 1) {
+            val i = ++index
+            currentQuestion = QuizzieDatabaseHelper.questions[i]
+        } else {
+            currentQuestion = QuizzieDatabaseHelper.questions[0]
+            navController.navigate(Screen.Result.route)
+        }
+    }
+
     Column(verticalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxSize().padding(vertical = 24.dp, horizontal = 8.dp)) {
         RadioButtonQuestion(currentQuestion)
 
         Row(modifier = Modifier.fillMaxWidth(),  horizontalArrangement = Arrangement.SpaceAround, verticalAlignment = Alignment.CenterVertically) {
-            RoundedButton(Color.Red, Icons.Default.Close, Color.Red, onClickAction = {currentQuestion = questions[--index]})
-            RoundedButton(Color.Blue, Icons.Default.Home, Color.Blue, size = 60.dp, onClickAction = {currentQuestion = questions[--index]})
-            RoundedButton(Color.Green, Icons.Default.Check, Color.Green, onClickAction = {currentQuestion = questions[++index]})
+            RoundedButton(Color.Red, Icons.Default.Close, Color.Red, onClickAction = {currentQuestion = QuizzieDatabaseHelper.questions[++index]})
+            RoundedButton(Color.Blue, Icons.Default.Home, Color.Blue, size = 60.dp, onClickAction = {navController.navigate(Screen.Home.route)})
+            RoundedButton(Color.Green, Icons.Default.Check, Color.Green, onClickAction = { handleNext() })
         }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun QuizScreenPreview() {
-    QuizzieTheme {
-        QuizScreen()
     }
 }
